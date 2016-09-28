402018b1faf0b6432b8a9f6cc240f952ee8c5fb5
Marc <marcjb2@gmail.com>
2016-09-28 20:21:59 +0200

    Interface actualizada

--- src/CompanyManager.java
+++ src/CompanyManager.java
@@ -1,11 +1,15 @@
+import java.util.Vector;
+
 /**
  * Created by Marc on 26/9/2016.
  */
-public interface CompanyManager<E> {
+public interface CompanyManager{
 
     public void addDept(String name, String description);
     public void addEmployee(String DNI, String name, double salary, String dept);
     public void addSalesMan(String DNI, String name, double salary, String dept);
     public void addDirector(String DNI, String name, double salary, String dept);
+    public Vector deps();
+    
 
 }
