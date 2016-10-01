e05267c70a5dbfa092d796886f98fcafade2a288
xarilo <xavier.rins@gmail.com>
2016-10-01 17:55:19 +0200

    Pidiendo de teclado los datos para crear un nuevo departamento

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
 /**
  * Created by Marc on 28/9/2016.
  */
-public class Main {
-    public static void main(String[] args){
-        //pruebagit
-        ComManIMpls cm = new ComManIMpls();
-        cm.addDept("Barnaclub","La mejor empresa del mundo mundial.");
-        cm.addDept("Adria","L'Adria mola, un xic.");
-        cm.deps();
-        cm.addEmployee("Marc", "45637X", 1340.56, "Barnaclub");
-        cm.Employees();
-        //añadiré un menú para pedir datos por teclado y recogerlos, o exponer la situacion de la empresa.
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class Main extends ComManIMpls{
+    static ComManIMpls nueva= new ComManIMpls();
+    public static void main(String[] args) throws IOException {
+
+        int op = 5;
+        while (op != 0) {
+            System.out.println("\n\n BIENVENIDO:");
+            System.out.print("Seleccione la opción que desea:");
+            System.out.println("1.- Crear departamento");
+            System.out.println("2.- Añadir empleado");
+            System.out.println("3.- Faltan añadir cosas");
+            System.out.println("0.- Salir");
+
+            op = getInt();
+            switch (op) {
+                case 1:
+                    System.out.println("Introduzca el nombre del departamento:");
+                    String nombre= getString();
+                    System.out.println("Escriba una breve descripcion de referencia:");
+                    String descripcion=getString();
+                    nueva.addDept(nombre, descripcion);
+                    System.out.println("El siguiente departamento ha sido añadido correctamente: "+nombre);
+                    break;
+
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    ;
+                    break;
+            }
+        }
+            ComManIMpls cm = new ComManIMpls();
+            cm.addDept("Barnaclub", "La mejor empresa del mundo mundial.");
+            cm.addDept("Adria", "L'Adria mola, un xic.");
+            cm.deps();
+            cm.addEmployee("Marc", "45637X", 1340.56, "Barnaclub");
+            cm.Employees();
+            //añadiré un menú para pedir datos por teclado y recogerlos, o exponer la situacion de la empresa.
+
+        }
+
+    // Funcion para capturar una cadena desde el teclado
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
     }
-}
+
+    // Funcion para capturar un entero desde el teclado
+    public static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+    }
